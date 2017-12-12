6e70752282d5bf3b0c0e7bd070f76f1d5b5d9404
田原 一樹 <ssp0727.lnc@gmail.com>
2017-12-12 13:51:26 +0900

    静的ファイルの供給／モジュール化／ejsについてのsampleを追加

--- sample1/index.js
+++ sample1/index.js
@@ -1,8 +1,8 @@
 var http = require('http');
 var fs = require('fs');
 
-var server = http.createServer(function(req,res){
-  fs.readFile('./temp.html','utf-8',function(err,data){
+var server = http.createServer((req,res) => {
+  fs.readFile('./temp.html','utf-8',(err,data)=>{
     res.writeHead(200,{'Content-Type':'text/html'});
     res.write(data);
     res.end();
